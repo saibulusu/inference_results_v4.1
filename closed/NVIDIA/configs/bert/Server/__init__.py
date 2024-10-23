88d6636db1a131d33a4c6723aa692007920c1e80
Sai Bulusu <sai.bulusu@gmail.com>
2024-10-23 12:57:36 -0700

    Update __init__.py

--- closed/NVIDIA/configs/bert/Server/__init__.py
+++ closed/NVIDIA/configs/bert/Server/__init__.py
@@ -554,16 +554,18 @@ class A100_SXM_80GB_MIG_1x1g10gb_HighAccuracy_TritonUnified(A100_SXM_80GB_MIG_1x
 class A100_SXM4_40GBx8(ServerGPUBaseConfig):
     system = KnownSystem.A100_SXM4_40GBx8
     active_sms = 60
-    gpu_batch_size = 96
+    gpu_batch_size = {'bert': 96}
     graphs_max_seqlen = 240
     server_num_issue_query_threads = 0
     server_target_qps = 24750
     soft_drop = 0.99
+    gpu_copy_streams = 4
+    gpu_inference_streams = 2
 
 
 @ConfigRegistry.register(HarnessType.Custom, AccuracyTarget.k_99_9, PowerSetting.MaxP)
 class A100_SXM4_40GBx8_HighAccuracy(A100_SXM4_40GBx8):
-    gpu_batch_size = 64
+    gpu_batch_size = {'bert': 64}
     precision = "fp16"
     server_target_qps = 11500
 
